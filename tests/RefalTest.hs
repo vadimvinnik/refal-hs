aec2c297f389911916e2a0dbc8c3a6ee77ffbf69
Vadim Vinnik <vadim.vinnik@gmail.com>
2016-12-04 22:20:21 +0000

    Create test data

--- tests/RefalTest.hs
+++ tests/RefalTest.hs
@@ -7,6 +7,26 @@ import Test.Framework
 
 {-# ANN module "HLint: ignore Use camelCase" #-}
 
+type TestObjectExpr = ObjectExpr Char
+type TestPatternExpr = PatternExpr String Char
+type TestActiveExpr = ActiveExpr String String Char
+
+type TestPatternItem = PatternItem String Char
+type TestActiveItem = ActiveItem String String Char
+
+type TestSentence = Sentence String String Char
+
+type TestFunctionBody = FunctionBody String String Char
+
+type TestModule = Module String String Char
+
+object_expr_0 = empty
+object_expr_1 = block empty
+object_expr_2 = (block empty) .+. (block empty)
+object_expr_3 = (block empty) .+. (block ((block empty) .+. (block empty)))
+object_expr_4 = quote "ab"
+object_expr_5 = (quote "ab") .+. (block $ quote "cd") .+. (quote "ef")
+
 test_stub =
     do
         assertBool  (True)
