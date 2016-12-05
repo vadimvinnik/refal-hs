d6dec2b194e7be4b98f7f11e3a0558bca7d54d5c
Vadim Vinnik <vadim.vinnik@gmail.com>
2016-12-05 00:07:15 +0000

    Add the 1st test

--- tests/RefalTest.hs
+++ tests/RefalTest.hs
@@ -2,6 +2,7 @@
 
 module RefalTest where
 
+import Data.String.ToString
 import Language.Refal
 import Test.Framework
 
@@ -20,7 +21,7 @@ type TestFunctionBody = FunctionBody String String Char
 
 type TestModule = Module String String Char
 
-object_expr_0 = empty
+object_expr_0 = empty :: TestObjectExpr
 object_expr_1 = block empty
 object_expr_2 = (block empty) .+. (block empty)
 object_expr_3 = (block empty) .+. (block ((block empty) .+. (block empty)))
@@ -29,5 +30,4 @@ object_expr_5 = (quote "ab") .+. (block $ quote "cd") .+. (quote "ef")
 
 test_stub =
     do
-        assertBool  (True)
-        assertEqual 0 0
+        assertEqual (toString object_expr_0) ""
